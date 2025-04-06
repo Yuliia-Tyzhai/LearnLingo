e67d8aefdbac6032d8e36642e22ebf85fd5ed556
Yuliia <yuliadzyoba@gmail.com>
2025-04-07 01:13:21 +0300

    add complete HomePage

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -27,60 +27,56 @@ const Navigation = () => {
   return (
     <header className={styles.header}>
       <div className={styles.headerContainer}>
-        <div className={styles.learnLingoLogoContainer}>
-          <Link to="/">
+        {/* Навігація: Лого, Home, Teachers */}
+        <nav className={styles.navigationContainer}>
+          <Link to="/" className={styles.learnLingoLogoContainer}>
             <ReactSVG className={styles.learnLingoLogo} src={learnLingoLogo} />
             <span className={styles.learnLingoText}>LearnLingo</span>
           </Link>
-        </div>
+          <ul className={styles.navList}>
+            <li className={styles.navListItem}>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  clsx(
+                    styles.link,
+                    isActive && !isTeachersPageCheck && styles.active
+                  )
+                }
+                end
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className={styles.navListItem}>
+              <NavLink
+                to="/teachers"
+                className={({ isActive }) =>
+                  clsx(
+                    styles.link,
+                    isActive && !isTeachersPageCheck && styles.active
+                  )
+                }
+                end
+              >
+                Teachers
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
 
-        <div>
-          <nav className={styles.nav}>
-            <ul className={styles.navList}>
-              <li className={styles.navListItem}>
-                <NavLink
-                  to="/"
-                  className={({ isActive }) =>
-                    clsx(
-                      styles.link,
-                      isActive && !isTeachersPageCheck && styles.active
-                    )
-                  }
-                  end
-                >
-                  Home
-                </NavLink>
-              </li>
-              <li className={styles.navListItem}>
-                <NavLink
-                  to="/teachers"
-                  className={({ isActive }) =>
-                    clsx(
-                      styles.link,
-                      isActive && !isTeachersPageCheck && styles.active
-                    )
-                  }
-                  end
-                >
-                  Teachers
-                </NavLink>
-              </li>
-              <li className={styles.navListItem}>
-                <button className={styles.loginBtn} onClick={openLoginModal}>
-                  <ReactSVG src={loginIcon} className={styles.loginIcon} />
-                  Log In
-                </button>
-              </li>
-              <li className={styles.navListItem}>
-                <button
-                  className={styles.registrationBtn}
-                  onClick={openRegistrationModal}
-                >
-                  Registration
-                </button>
-              </li>
-            </ul>
-          </nav>
+        {/* Кнопки: Log In, Registration */}
+        <div className={styles.buttonsContainer}>
+          <button className={styles.loginBtn} onClick={openLoginModal}>
+            <ReactSVG src={loginIcon} className={styles.loginIcon} />
+            Log In
+          </button>
+          <button
+            className={styles.registrationBtn}
+            onClick={openRegistrationModal}
+          >
+            Registration
+          </button>
         </div>
       </div>
 
@@ -90,7 +86,7 @@ const Navigation = () => {
             <h2 className={styles.loginTitle}>Log In</h2>
             <p className={styles.loginText}>
               Welcome back! Please enter your credentials to access your account
-              and continue your search for an teacher.
+              and continue your search for a teacher.
             </p>
           </div>
 
@@ -105,7 +101,7 @@ const Navigation = () => {
             <p className={styles.registerText}>
               Thank you for your interest in our platform! In order to register,
               we need some information. Please provide us with the following
-              information
+              details.
             </p>
           </div>
 
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,48 @@
 import React from 'react';
+import styles from './HomePage.module.css';
+import homePageImg from '../../assets/home-page-img.png';
 
 const HomePage = () => {
   return (
-    <div>
-      <h2>HomePage</h2>
+    <div className={styles.pageContainer}>
+      <div className={styles.firstContainer}>
+        <div className={styles.titleContainer}>
+          <h1 className={styles.homePageTitle}>
+            Unlock your potential with the best{' '}
+            <span className={styles.titleSpan}>language</span> tutors
+          </h1>
+          <p className={styles.homePageText}>
+            Embark on an Exciting Language Journey with Expert Language Tutors:
+            Elevate your language proficiency to new heights by connecting with
+            highly qualified and experienced tutors.
+          </p>
+          <button className={styles.homePageBtn}>Get started</button>
+        </div>
+        <div>
+          <img src={homePageImg} alt="Language learning" />
+        </div>
+      </div>
+
+      <div className={styles.listContainer}>
+        <ul>
+          <li>
+            <h2 className={styles.listItemTitle}>32,000 +</h2>
+            <p className={styles.listItemText}>Experienced tutors</p>
+          </li>
+          <li>
+            <h2 className={styles.listItemTitle}>300,000 +</h2>
+            <p className={styles.listItemText}>5-star tutor reviews</p>
+          </li>
+          <li>
+            <h2 className={styles.listItemTitle}>120 +</h2>
+            <p className={styles.listItemText}>Subjects taught</p>
+          </li>
+          <li>
+            <h2 className={styles.listItemTitle}>200 +</h2>
+            <p className={styles.listItemText}>Tutor nationalities</p>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
