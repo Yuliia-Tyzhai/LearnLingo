d94ea685d614bc161bf7b74a6a329da68331c6a1
Yuliia <yuliadzyoba@gmail.com>
2025-04-06 15:35:39 +0300

    add pages

---
+++ src/pages/FavoritesPage/FavoritesPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FavoritesPage = () => {
+  return (
+    <div>
+      <h2>FavoritesPage</h2>
+    </div>
+  );
+};
+
+export default FavoritesPage;
---
+++ src/pages/TeachersPage/TeachersPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TeachersPage = () => {
+  return (
+    <div>
+      <h2>TeachersPage</h2>
+    </div>
+  );
+};
+
+export default TeachersPage;
