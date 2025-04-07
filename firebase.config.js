97d42a0c68a1d4c753dbe20b63a67665d6bdcaf9
Yuliia <yuliadzyoba@gmail.com>
2025-04-07 23:55:50 +0300

    add fixing firebase.config.js

--- firebase.config.js
+++ firebase.config.js
@@ -1,22 +1,27 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: 'AIzaSyAThqJTZGHgAzl51GvLVFqLjXT1xMQm9pE',
   authDomain: 'teachers-a5542.firebaseapp.com',
   databaseURL: 'https://teachers-a5542-default-rtdb.firebaseio.com',
   projectId: 'teachers-a5542',
-  storageBucket: 'teachers-a5542.firebasestorage.app',
+  storageBucket: 'teachers-a5542.appspot.com',
   messagingSenderId: '330202288298',
   appId: '1:330202288298:web:21acd8584b26e28a0900d3',
   measurementId: 'G-K42775TSZ2',
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const database = getDatabase(app);
+const firestore = getFirestore(app);
+const storage = getStorage(app);
+
+export { app, analytics, auth, database, firestore, storage };
