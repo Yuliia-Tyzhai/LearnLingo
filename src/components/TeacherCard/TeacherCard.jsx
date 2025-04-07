4303144714dee81fdf8038a43f8d31c72700dcb9
Yuliia <yuliadzyoba@gmail.com>
2025-04-08 00:55:41 +0300

    add TeacherCard

--- src/components/TeacherCard/TeacherCard.jsx
+++ src/components/TeacherCard/TeacherCard.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
-const TeacherCard = () => {
-  return <div></div>;
+const TeacherCard = ({ teacher, onFavoriteToggle }) => {
+  return (
+    <div className="teacher-card">
+      <img
+        src={teacher.avatar_url}
+        alt={`${teacher.name} ${teacher.surname}`}
+      />
+      <h2>{`${teacher.name} ${teacher.surname}`}</h2>
+      <p>Languages: {teacher.languages.join(', ')}</p>
+      <p>Levels: {teacher.levels.join(', ')}</p>
+      <p>Price per hour: ${teacher.price_per_hour}</p>
+      <button onClick={() => onFavoriteToggle(teacher.id)}>❤️</button>
+      <button>Read More</button>
+      <button>Book Trial Lesson</button>
+    </div>
+  );
 };
 
 export default TeacherCard;
